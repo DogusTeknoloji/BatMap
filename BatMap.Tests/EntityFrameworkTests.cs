1a55c34c5ae8bf66d0f625e2a6ed2b5024dbf481
Umut Ozel <umutozel@gmail.com>
2017-04-17 21:27:25 +0300

    Removing unnecessary mock setups

--- BatMap.Tests/EntityFrameworkTests.cs
+++ BatMap.Tests/EntityFrameworkTests.cs
@@ -154,25 +154,7 @@ namespace BatMap.Tests {
             mockList.As<IQueryable<T>>().Setup(m => m.Expression).Returns(queryable.Expression);
             mockList.As<IQueryable<T>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
             mockList.As<IQueryable<T>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
-            mockList.Setup(m => m.Include(It.IsAny<string>())).Returns(mockList.Object);
-            mockList.Setup(m => m.Local).Returns(list);
-            mockList.Setup(m => m.Add(It.IsAny<T>())).Returns((T a) => {
-                list.Add(a);
-                return a;
-            });
-            mockList.Setup(m => m.AddRange(It.IsAny<IEnumerable<T>>())).Returns((IEnumerable<T> a) => {
-                foreach (var item in a.ToArray()) list.Add(item);
-                return a;
-            });
-            mockList.Setup(m => m.Remove(It.IsAny<T>())).Returns((T a) => {
-                list.Remove(a);
-                return a;
-            });
-            mockList.Setup(m => m.RemoveRange(It.IsAny<IEnumerable<T>>())).Returns((IEnumerable<T> a) => {
-                foreach (var item in a.ToArray()) list.Remove(item);
-                return a;
-            });
-
+            
             return mockList;
         }
     }
